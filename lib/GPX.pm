1e363c0c9a74e50eeea2286e184f8cceb092d2bc
Hamish Coleman <hamish@zot.org>
2018-02-09 21:22:55 +1100

    Start adding the GPX output state machine

--- lib/GPX.pm
+++ lib/GPX.pm
@@ -16,4 +16,66 @@ sub new {
     return $self;
 }
 
+sub _output_gpx_head {
+    my ($self, $oldstate, $newstate) = @_;
+    my $output;
+    $output .= "<gpx\n";
+    $output .= " creator=\"HC GPX.pm\"\n";
+    $output .= " version=\"1.1\"\n";
+    $output .= " xmlns=\"http://www.topografix.com/GPX/1/1\"\n";
+    $output .= " xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\">\n";
+
+    $self->{state} = $newstate;
+
+    return $output;
+}
+
+sub _output_trk_head {
+    my ($self, $oldstate, $newstate) = @_;
+    my $output;
+    $output .= " <trk>\n";
+    $output .= "  <name>" . $self->{trk}{name} . "</name>\n";
+
+    $self->{state} = $newstate;
+
+    return $output;
+}
+
+my $states = {
+    'empty' => {
+        'have_trk_name' => \&_output_gpx_head,
+    },
+    'have_trk_name' => {
+        'have_trkseg' => \&_output_trk_head,
+    },
+};
+
+# Change the current object state, which could flush some old data
+sub _state {
+    my ($self,$newstate) = @_;
+    my $oldstate = $self->{state};
+
+    if (!defined($states->{$oldstate})) {
+        die("Cannot transition from state $oldstate");
+    }
+    if (!defined($states->{$oldstate}{$newstate})) {
+        die("Cannot transition from state $oldstate to $newstate");
+    }
+
+    return $states->{$oldstate}{$newstate}($self, $oldstate, $newstate);
+}
+
+# record the name for this trk
+sub _add_trk_name {
+    my ($self,$name) = @_;
+
+    $self->{trk}{name} = $name;
+    return $self->_state('have_trk_name');
+}
+
+sub _add_trkseg {
+    my ($self) = @_;
+    return $self->_state('have_trkseg');
+}
+
 1;
--- lib/GPX.t
+++ lib/GPX.t
@@ -4,7 +4,22 @@ use Test::More 'no_plan';
 my $class = 'GPX';
 use_ok($class);
 
+# TODO
+# - call the _state method and confirm that transitioning from an invalid
+#   state, or trying an invalide from/to pair does actually die
+
 my $obj = $class->new();
 isa_ok($obj, $class);
 
+my $expect_gpx_head = "<gpx\n" .
+    " creator=\"HC GPX.pm\"\n" .
+    " version=\"1.1\"\n" .
+    " xmlns=\"http://www.topografix.com/GPX/1/1\"\n" .
+    " xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\">\n";
 
+is($obj->_add_trk_name('larry'), $expect_gpx_head);
+
+my $expect_trk_head = " <trk>\n" .
+    "  <name>larry</name>\n";
+
+is($obj->_add_trkseg(), $expect_trk_head);
